dd6a3caae40ed6ec134af090fc4302aed3da72ba
FlouffyDaWolf <evanmideo@hotmail.com>
2025-11-10 09:45:26 +0100

    uyf

--- Assets/Components/ProceduralGeneration/3_Noise/Noise.cs
+++ Assets/Components/ProceduralGeneration/3_Noise/Noise.cs
@@ -1,59 +1,161 @@
+using System;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using Components.ProceduralGeneration;
 using Cysharp.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 [CreateAssetMenu(menuName = "Procedural Generation Method/Noise")]
 public class Noise : ProceduralGenerationMethod
 {
-    [Header("Noise Parameters")]
+    [Header("Roof Noise Parameters")]
     public FastNoiseLite.NoiseType noiseType = FastNoiseLite.NoiseType.OpenSimplex2;
     [Range(0f, 1f)] public float frequency = 0.04f;
     [Range(0f, 1f)] public float amplitude = 0.981f;
 
-    [Header("Fractal Parameters")]
+    [Header("Roof Fractal Parameters")]
     public FastNoiseLite.FractalType fractalType = FastNoiseLite.FractalType.None;
     [Range(1, 10)] public int octaves = 2;
     [Range(0f, 5f)] public float lacunarity = 1.2f;
     [Range(0f, 1f)] public float persistence = 0.5f;
 
+    [Header("Floor Noise Parameters")]
+    public FastNoiseLite.NoiseType floornoiseType = FastNoiseLite.NoiseType.OpenSimplex2;
+    [Range(0f, 1f)] public float floorfrequency = 0.04f;
+    [Range(0f, 1f)] public float flooramplitude = 0.981f;
+
+    [Header("Floor Fractal Parameters")]
+    public FastNoiseLite.FractalType floorfractalType = FastNoiseLite.FractalType.None;
+    [Range(1, 10)] public int flooroctaves = 2;
+    [Range(0f, 5f)] public float floorlacunarity = 1.2f;
+    [Range(0f, 1f)] public float floorpersistence = 0.5f;
+
     [Header("Heights")]
-    [Range(-1f, 1f)] public float waterHeight = -0.71f;
-    [Range(-1f, 1f)] public float sandHeight = -0.48f;
-    [Range(-1f, 1f)] public float grassHeight = 0.6f;
-    [Range(-1f, 1f)] public float rockHeight = 1.0f;
+    [Range(0, 20)] public int offset = 0;
+
+    Mesh mesh;
+    Mesh roofMesh;
+    GameObject floor;
+    GameObject roof;
+    public Material mat;
+
+    FastNoiseLite noiseRoof;
+    FastNoiseLite noiseFloor;
 
     protected override async UniTask ApplyGeneration(CancellationToken cancellationToken)
     {
         cancellationToken.ThrowIfCancellationRequested();
 
-        FastNoiseLite noise = new FastNoiseLite();
-        noise.SetNoiseType(noiseType);
-        noise.SetFrequency(frequency);
-        noise.SetDomainWarpAmp(amplitude);
+        noiseRoof = new FastNoiseLite();
+        noiseRoof.SetNoiseType(noiseType);
+        noiseRoof.SetFrequency(frequency);
+        noiseRoof.SetDomainWarpAmp(amplitude);
 
-        noise.SetFractalType(fractalType);
-        noise.SetFractalOctaves(octaves);
-        noise.SetFractalLacunarity(lacunarity);
+        noiseRoof.SetFractalType(fractalType);
+        noiseRoof.SetFractalOctaves(octaves);
+        noiseRoof.SetFractalLacunarity(lacunarity);
+        noiseRoof.SetFractalGain(persistence);
+
+        noiseFloor = new FastNoiseLite();
+        noiseFloor.SetNoiseType(floornoiseType);
+        noiseFloor.SetFrequency(floorfrequency);
+        noiseFloor.SetDomainWarpAmp(flooramplitude);
+
+        noiseFloor.SetFractalType(floorfractalType);
+        noiseFloor.SetFractalOctaves(flooroctaves);
+        noiseFloor.SetFractalLacunarity(floorlacunarity);
+        noiseFloor.SetFractalGain(floorpersistence);
+
+        DestroyImmediate(GameObject.Find("floor"));
+
+        mesh = new Mesh();
+        mesh.name = "floor";
+        mesh.indexFormat = IndexFormat.UInt32;
+        floor = new GameObject("floor", typeof(MeshRenderer), typeof(MeshFilter));
+
+        floor.GetComponent<MeshFilter>().mesh = mesh;
+        floor.GetComponent<MeshRenderer>().material = mat;
+
+        List<Vector3> floorVertices = new List<Vector3>();
+        List<int> floorTriangles = new List<int>();
+
+        List<Vector3> roofVertices = new List<Vector3>();
+        List<int> roofTriangles = new List<int>();
 
         for (int x = 0; x < Grid.Width; x++)
         {
             for (int y = 0; y < Grid.Lenght; y++)
             {
-                Grid.TryGetCellByCoordinates(x, y, out var cell);
+                float floorHeight = -(noiseFloor.GetNoise(x, y) + 1f) * 10f + (int)Math.Floor((float)offset / 2);
+                float roofHeight = (noiseRoof.GetNoise(x, y) + 1f) * 10f - (int)Math.Ceiling((float)offset / 2);
 
-                if (noise.GetNoise(x, y) <= waterHeight)
-                    AddTileToCell(cell, WATER_TILE_NAME, true);
-                else if (noise.GetNoise(x, y) <= sandHeight)
-                    AddTileToCell(cell, SAND_TILE_NAME, true);
-                else if (noise.GetNoise(x, y) <= grassHeight)
-                    AddTileToCell(cell, GRASS_TILE_NAME, true);
-                else if (noise.GetNoise(x, y) <= rockHeight)
-                    AddTileToCell(cell, ROCK_TILE_NAME, true);
+                if (Math.Abs(roofHeight - floorHeight) <= 3 
+                    || y == Grid.Lenght - 1
+                    || y == 0
+                    || x == Grid.Width - 1
+                    || x == 0)
+                {
+                    float joinedHeight = (floorHeight + roofHeight) / 2f;
+    
+                    floorVertices.Add(new Vector3(x, joinedHeight, y));
+                    roofVertices.Add(new Vector3(x, joinedHeight, y));
+                }
+                else
+                {
+                    floorVertices.Add(new Vector3(x, floorHeight, y));
+                    roofVertices.Add(new Vector3(x, roofHeight, y));
+                }
             }
         }
+
+        for (int x = 0; x < Grid.Width - 1; x++)
+        {
+            for (int y = 0; y < Grid.Lenght - 1; y++)
+            {
+                int idx = x * Grid.Lenght + y;
+
+                floorTriangles.Add(idx);
+                floorTriangles.Add(idx + 1);
+                floorTriangles.Add(idx + Grid.Lenght);
+
+                floorTriangles.Add(idx + 1);
+                floorTriangles.Add(idx + Grid.Lenght + 1);
+                floorTriangles.Add(idx + Grid.Lenght);
+
+                roofTriangles.Add(idx);
+                roofTriangles.Add(idx + Grid.Lenght);
+                roofTriangles.Add(idx + 1);
+
+                roofTriangles.Add(idx + 1);
+                roofTriangles.Add(idx + Grid.Lenght);
+                roofTriangles.Add(idx + Grid.Lenght + 1);
+            }
+        }
+
+        List<Vector3> combinedVertices = new List<Vector3>();
+        combinedVertices.AddRange(floorVertices);
+        combinedVertices.AddRange(roofVertices);
+
+        List<int> combinedTriangles = new List<int>();
+        combinedTriangles.AddRange(floorTriangles);
+
+        int vertexOffset = floorVertices.Count;
+        for (int i = 0; i < roofTriangles.Count; i++)
+        {
+            combinedTriangles.Add(roofTriangles[i] + vertexOffset);
+        }
+        
+        mesh.Clear();
+        mesh.vertices = combinedVertices.ToArray();
+        mesh.triangles = combinedTriangles.ToArray();
+        mesh.RecalculateBounds();
+        mesh.RecalculateNormals();
+        mesh.RecalculateTangents();
+
+        MeshCollider collider = floor.AddComponent<MeshCollider>();
+        collider.sharedMesh = mesh;
 
         await UniTask.Delay(GridGenerator.StepDelay, cancellationToken: cancellationToken);
     }
